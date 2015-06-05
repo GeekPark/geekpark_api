197007714f849d47275ce6644c6504782164fc69
Gavin <gavin.li1986@gmail.com>
2015-06-05 10:27:06 +0800

    fix activity find

--- lib/geekpark_api/activities.rb
+++ lib/geekpark_api/activities.rb
@@ -2,7 +2,7 @@ module GeekparkApi
   class Activities
     class << self
       def find id
-        res = Faraday.get "#{GeekparkApi.config.event_api_base_uri}/#{id}"
+        res = Faraday.get "#{GeekparkApi.config.event_api_base_uri}/activities/#{id}"
 
         {status: res.status, body: JSON.parse(res.body)}
       end
