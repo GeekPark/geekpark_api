9bea5cc9212fcb7ee0f8c2f40e933c3aa201ac46
Gavin <gavin.li1986@gmail.com>
2015-06-10 15:48:19 +0800

    update to 0.1.3 add update open_id api

--- lib/geekpark_api/user.rb
+++ lib/geekpark_api/user.rb
@@ -10,5 +10,14 @@ module GeekparkApi
       {status: res.status, body: JSON.parse(res.body)}
     end
 
+    def self.event_open_id token, openid
+      res = Faraday.patch "#{GeekparkApi.config.user_api_base_uri}/event_open_id" do |req|
+        req.headers['Authorization'] = "Bearer #{token}"
+        req.body = {open_id: openid}
+      end
+
+      {status: res.status, body: JSON.parse(res.body)}
+    end
+
   end
 end
