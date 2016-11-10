45e703ee1d0f7bf90c2b269b90fbf1acb3a025c5
Z Kun <zhukun6150909@outlook.com>
2016-11-10 16:56:02 +0800

    add locale params

--- lib/geekpark_api/activities.rb
+++ lib/geekpark_api/activities.rb
@@ -1,8 +1,8 @@
 module GeekparkApi
   class Activities
     class << self
-      def find id
-        res = Faraday.get "#{GeekparkApi.config.event_api_base_uri}/activities/#{id}"
+      def find id, params = {}
+        res = Faraday.get "#{GeekparkApi.config.event_api_base_uri}/activities/#{id}", params
 
         {status: res.status, body: JSON.parse(res.body)}
       end
